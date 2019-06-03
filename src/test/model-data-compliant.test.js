38817b8927be7a8d840c0cc1a2e060f27a647f20
Romain Goussu <rgoussu.develop@gmail.com>
2019-06-03 22:57:03 +0200

    feat(data-model): refactored model to make data model distinct from object model

--- src/test/model-data-compliant.test.js
+++ src/test/model-data-compliant.test.js
@@ -3,25 +3,26 @@
 const Ajv = require('ajv');
 const fs = require('fs');
 const path = require('path');
-const schemaLocation = path.join(__dirname, "../schemas", "model-data-schema.json");
-const modelLocation = path.join(__dirname, "data/compliant-model.json");
+const schemaLocation = [path.join(__dirname, "../schemas", "model-data-schema.json"),path.join(__dirname, "../schemas", "common-schema.json")];
+const modelLocation = path.join(__dirname, "data/compliant-data-model.json");
 //TODO : improves those tests by using a js test framework
 
 // test models
-const model = JSON.parse(fs.readFileSync(modelLocation));
+const schema = [JSON.parse(fs.readFileSync(schemaLocation[0])),JSON.parse(fs.readFileSync(schemaLocation[1]))];
 
 // schema
-const schema = JSON.parse(fs.readFileSync(schemaLocation));
+const model = JSON.parse(fs.readFileSync(modelLocation));
 
 // initializing
 const ajv =new Ajv({allErrors:true});
 
 // compiling model
-const schemaValide = ajv.compile(schema);
+ajv.addSchema(schema);
 
+const validate = ajv.getSchema("http://github.com/thc-tools/schemas/data-schema.json");
 
-const isModelValide = schemaValide(model);
-
+const isModelValide = validate(model);
+console.log(validate.errors)
 test("compliant model test => should be valid", ()=>{
     expect(isModelValide).toBe(true);
 })
--- src/test/model-data-non-compliant.test.js
+++ src/test/model-data-non-compliant.test.js
@@ -1,24 +1,24 @@
 const Ajv = require('ajv');
 const fs = require('fs');
 const path = require('path');
-const schemaLocation = path.join(__dirname, "../schemas", "model-data-schema.json");
-const modelLocation =  path.join(__dirname,"data/non-compliant-model.json");
+const schemaLocation = [path.join(__dirname, "../schemas", "model-data-schema.json"),path.join(__dirname, "../schemas", "common-schema.json")];
+const modelLocation = path.join(__dirname, "data/non-compliant-data-model.json");
+//TODO : improves those tests by using a js test framework
 
 // test models
-const model = JSON.parse(fs.readFileSync(modelLocation));
+const schema = [JSON.parse(fs.readFileSync(schemaLocation[1], "utf-8"),),JSON.parse(fs.readFileSync(schemaLocation[0],"utf-8"))];
 
 // schema
-const schema = JSON.parse(fs.readFileSync(schemaLocation));
+const model = JSON.parse(fs.readFileSync(modelLocation));
+
+
 
 // initializing
-const ajv =new Ajv({allErrors:true});
+const ajv =new Ajv({schemas: schema,allErrors:true});
 
-// compiling model
-const schemaValide = ajv.compile(schema);
+const validate = ajv.getSchema("http://github.com/thc-tools/schemas/data-schema.json");
+const isModelValide = validate(model);
 
-
-const isModelValide = schemaValide(model);
-
-test("compliant model test => should be valid", ()=>{
+test("non compliant model test => should not be valid", ()=>{
     expect(isModelValide).toBe(false);
 });
